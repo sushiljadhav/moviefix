0af75738ae32d7a3375e53dad16ae8c10b771321
sushiljadhav <sushilj76@gmail.com>
2024-05-27 04:15:16 +0530

    store and api setup

---
+++ src/components/genres/Genres.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { IGenreMap, ISelectOption } from "../../utils/api";
+import { MultiValue, SingleValue } from "react-select";
+import { useTypedSelector } from "../../hooks/useSelector";
+import Wrapper from "../global/wrapper/Wrapper";
+import "./genres.css";
+
+function Genres() {
+	const [genreOptions, setGenreOptions] = useState<ISelectOption[]>([]);
+	const [selectedGenre, setSelectedGenres] = useState<number[]>([]);
+	const { genres } = useTypedSelector((state) => state.movies);
+
+	const convertGenresToOptions = (genresMap: IGenreMap): ISelectOption[] => {
+		return Object.values(genresMap).map((genre) => ({
+			value: genre.id,
+			label: genre.name,
+		}));
+	};
+
+	const handleGenreClick = (genreValue: number) => {
+		setSelectedGenres((prevGenres) => {
+			if (prevGenres.includes(genreValue)) {
+				return prevGenres.filter((value) => value !== genreValue);
+			} else {
+				return [...prevGenres, genreValue];
+			}
+		});
+	};
+
+	console.log("selectedGenre", selectedGenre);
+
+	useEffect(() => {
+		if (genres) {
+			const convertedGenres = convertGenresToOptions(genres);
+			setGenreOptions(convertedGenres);
+		}
+	}, [genres]);
+	console.log("Genres", genres);
+	return (
+		<div className="genres">
+			<Wrapper>
+				<div className="genres_content">
+					{genreOptions.map((genre, index) => {
+						return (
+							<div
+								className={`genres_chip ${
+									selectedGenre.includes(genre.value)
+										? "active"
+										: ""
+								}`}
+								key={index}
+								onClick={() => handleGenreClick(genre.value)}
+							>
+								<span className="genres_name">
+									{genre.label}
+								</span>
+							</div>
+						);
+					})}
+				</div>
+			</Wrapper>
+		</div>
+	);
+}
+
+export default Genres;
---
+++ src/components/lazyImage/LazyLoadImages.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
+interface LazyLoadImagesProps {
+	src: string;
+	className?: string;
+	alt?: string;
+}
+
+function LazyLoadImages({ src, className, alt }: LazyLoadImagesProps) {
+	return (
+		<LazyLoadImage
+			className={className || ""}
+			alt={alt}
+			effect="blur"
+			src={src}
+		/>
+	);
+}
+
+export default LazyLoadImages;
